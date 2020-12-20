f71acfbac7e2c7cbe97172b2a930a5f8095eb3ec
Nikola Savic <nikolasavic@gmail.com>
2020-12-20 15:31:43 -0500

    refactor project structure

---
+++ dynamic_programming/countStairs.py
@@ -0,0 +1,41 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+#
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+# Example 1:
+#
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+#
+# Example 2:
+#
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+
+class Solution:
+    def climbStairs(self, n: int, memo={}) -> int:
+        if n in memo:
+            return memo[n]
+
+        if n == 0:
+            return 1
+
+        if n <= 0:
+            return 0
+
+        memo[n] = self.climbStairs(n - 1, memo=memo) + self.climbStairs(
+            n - 2, memo=memo
+        )
+
+        return memo[n]
+
+
+if __name__ == "__main__":
+    assert Solution().climbStairs(5) == 8
+    assert Solution().climbStairs(45) == 1836311903
