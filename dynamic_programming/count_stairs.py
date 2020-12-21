b600522e69f1f40d7c842b27fc294aecfd2d3721
Nikola Savic <nikolasavic@gmail.com>
2020-12-20 19:22:41 -0500

    Rename and remove clutter

--- dynamic_programming/count_stairs.py
+++ dynamic_programming/count_stairs.py
@@ -18,24 +18,22 @@
 # 2. 1 step + 2 steps
 # 3. 2 steps + 1 step
 
-class Solution:
-    def climbStairs(self, n: int, memo={}) -> int:
-        if n in memo:
-            return memo[n]
 
-        if n == 0:
-            return 1
-
-        if n <= 0:
-            return 0
-
-        memo[n] = self.climbStairs(n - 1, memo=memo) + self.climbStairs(
-            n - 2, memo=memo
-        )
-
+def climbStairs(n, memo={}):
+    if n in memo:
         return memo[n]
+
+    if n == 0:
+        return 1
+
+    if n <= 0:
+        return 0
+
+    memo[n] = climbStairs(n - 1, memo=memo) + climbStairs(n - 2, memo=memo)
+
+    return memo[n]
 
 
 if __name__ == "__main__":
-    assert Solution().climbStairs(5) == 8
-    assert Solution().climbStairs(45) == 1836311903
+    assert climbStairs(5) == 8
+    assert climbStairs(45) == 1836311903
