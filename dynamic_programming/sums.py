22ae9812962a8ce06c62a82c1a24224a8c5b2b1b
Nikola Savic <nikolasavic@gmail.com>
2020-12-21 09:34:14 -0500

    Memoized can_sum()

--- dynamic_programming/sums.py
+++ dynamic_programming/sums.py
@@ -1,22 +1,39 @@
-def can_sum(target, numbers):
+# Given a target, return whether it's possible to create a combination of numbers provided
+# that sum to the target
+# Example:
+# target: 7, numbers: [2,3,5]
+# output: true
+
+
+def can_sum(target, numbers, memo=None):
+    if memo is None:
+        memo = {}
+    if target in memo:
+        return memo[target]
+
     if target == 0:
         return True
-
     if target < 0:
         return False
 
     for n in numbers:
         remainder = target - n
-        if can_sum(remainder, numbers):
+        if can_sum(remainder, numbers, memo):   
+            memo[remainder] = True
             return True
 
+    memo[target] = False
     return False
 
 
-if __name__ == "__main__":
+def run_can_sum():
     assert can_sum(5, [3, 4]) == False
     assert can_sum(7, [2, 3, 5, 7]) == True
     assert can_sum(8, [2, 3, 5, 7]) == True
     assert can_sum(13, [2, 3, 5, 7]) == True
     assert can_sum(7, [2, 4]) == False
-    # assert can_sum(300, [7, 14]) == False
+    assert can_sum(300, [7, 14]) == False
+
+
+if __name__ == "__main__":
+    run_can_sum()
