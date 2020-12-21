006f31fd71f94012db228b58d0e89a00d8720b9d
Nikola Savic <nikolasavic@gmail.com>
2020-12-21 10:49:32 -0500

    Add memoized has_sum()

--- dynamic_programming/sums.py
+++ dynamic_programming/sums.py
@@ -3,6 +3,7 @@
 # Example:
 # target: 7, numbers: [2,3,5]
 # output: true
+from utils import assert_equal
 
 
 def can_sum(target, numbers, memo=None):
@@ -18,7 +19,7 @@ def can_sum(target, numbers, memo=None):
 
     for n in numbers:
         remainder = target - n
-        if can_sum(remainder, numbers, memo):   
+        if can_sum(remainder, numbers, memo):
             memo[remainder] = True
             return True
 
@@ -26,14 +27,45 @@ def can_sum(target, numbers, memo=None):
     return False
 
 
+def has_sum(target, numbers, memo=None):
+    if memo is None:
+        memo = {}
+    if target in memo:
+        return memo[target]
+
+    if target == 0:
+        return []
+    if target < 0:
+        return None
+
+    for n in numbers:
+        remainder = target - n
+        result = has_sum(remainder, numbers, memo)
+        if result is not None:
+            memo[remainder] = result
+            return [*result, n]
+
+    memo[target] = None
+    return None
+
+
 def run_can_sum():
-    assert can_sum(5, [3, 4]) == False
-    assert can_sum(7, [2, 3, 5, 7]) == True
-    assert can_sum(8, [2, 3, 5, 7]) == True
-    assert can_sum(13, [2, 3, 5, 7]) == True
-    assert can_sum(7, [2, 4]) == False
-    assert can_sum(300, [7, 14]) == False
+    assert_equal(can_sum(5, [3, 4]), False)
+    assert_equal(can_sum(7, [2, 3, 5, 7]), True)
+    assert_equal(can_sum(8, [2, 3, 5, 7]), True)
+    assert_equal(can_sum(13, [2, 3, 5, 7]), True)
+    assert_equal(can_sum(7, [2, 4]), False)
+    assert_equal(can_sum(300, [7, 14]), False)
+
+
+def run_has_sum():
+    assert_equal(has_sum(5, [3, 4]), None)
+    assert_equal(has_sum(7, [2, 3, 5, 7]), [3, 2, 2])
+    assert_equal(has_sum(8, [2, 3, 5, 7]), [2, 2, 2, 2])
+    assert_equal(has_sum(13, [3, 5, 7]), [7, 3, 3])
+    assert_equal(has_sum(7, [2, 4]), None)
+    assert_equal(has_sum(300, [7, 14]), None)
 
 
 if __name__ == "__main__":
-    run_can_sum()
+    run_has_sum()
