f834cacabf41412deaab73299a0495e614a410bc
Nikola Savic <nikolasavic@gmail.com>
2020-12-22 16:38:51 -0500

    Add LIS

--- dynamic_programming/longest_subsequence.py
+++ dynamic_programming/longest_subsequence.py
@@ -13,6 +13,27 @@ def lis_length(numbers):
     return max(log)
 
 
+def lis(numbers):
+    log = [1 for _ in range(len(numbers))]
+    prevs = [None for _ in range(len(numbers))]
+
+    for j in range(1, len(numbers)):
+        for i in range(j):
+            if numbers[i] < numbers[j]:
+                if log[j] < log[i] + 1:
+                    log[j] = log[i] + 1
+                    prevs[j] = i
+
+    max_ind = log.index(max(log))
+    seq = [numbers[max_ind]]
+    prev = prevs[max_ind]
+    while prev is not None:
+        seq.insert(0, numbers[prev])
+        prev = prevs[prev]
+
+    return seq
+
+
 def run_lis_length():
     assert_equal(lis_length([10, 9, 2, 5, 3, 7, 19, 18]), 4)
 
@@ -31,5 +52,23 @@ def run_lis_length():
     assert_equal(lis_length([50, 3, 10, 7, 40, 80]), 4)
 
 
+def run_lis():
+    assert_equal(lis([10, 9, 2, 5, 3, 7, 19, 18]), [2, 5, 7, 19])
+
+    assert_equal(lis([0, 1, 0, 3, 2, 3]), [0, 1, 2, 3])
+
+    assert_equal(lis([3, 10, 2, 1, 20]), [3, 10, 20])
+
+    assert_equal(lis([3, 2, 1]), [3])
+
+    assert_equal(lis([10, 22, 9, 33, 21, 50, 41, 60, 80]), [10, 22, 33, 50, 60, 80])
+
+    assert_equal(lis([7, 7, 7, 7, 7, 7, 7]), [7])
+
+    assert_equal(lis([2, 8, 1, 0, 4, 7, 10]), [2, 4, 7, 10])
+
+    assert_equal(lis([50, 3, 10, 7, 40, 80]), [3, 10, 40, 80])
+
+
 if __name__ == "__main__":
-    run_lis_length()
+    run_lis()
