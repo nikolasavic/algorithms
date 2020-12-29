5aa64d7ad36cfe095606e557c7840a2627da4324
Nikola Savic <nikolasavic@gmail.com>
2020-12-29 14:44:49 -0500

    Refactor names

--- dynamic_programming/sums.py
+++ dynamic_programming/sums.py
@@ -6,7 +6,7 @@
 from utils import assert_equal
 
 
-def can_sum(target, numbers, memo=None):
+def can_sum_memo(target, numbers, memo=None):
     if memo is None:
         memo = {}
     if target in memo:
@@ -19,7 +19,7 @@ def can_sum(target, numbers, memo=None):
 
     for n in numbers:
         remainder = target - n
-        if can_sum(remainder, numbers, memo):
+        if can_sum_memo(remainder, numbers, memo):
             memo[remainder] = True
             return True
 
@@ -27,7 +27,7 @@ def can_sum(target, numbers, memo=None):
     return False
 
 
-def has_sum(target, numbers, memo=None):
+def has_sum_memo(target, numbers, memo=None):
     if memo is None:
         memo = {}
     if target in memo:
@@ -40,7 +40,7 @@ def has_sum(target, numbers, memo=None):
 
     for n in numbers:
         remainder = target - n
-        result = has_sum(remainder, numbers, memo)
+        result = has_sum_memo(remainder, numbers, memo)
         if result is not None:
             memo[remainder] = result
             return [*result, n]
@@ -49,7 +49,7 @@ def has_sum(target, numbers, memo=None):
     return None
 
 
-def best_sum(target, numbers, memo=None):
+def best_sum_memo(target, numbers, memo=None):
     if memo is None:
         memo = {}
     if target in memo:
@@ -63,7 +63,7 @@ def best_sum(target, numbers, memo=None):
     shortest = None
     for n in numbers:
         remainder = target - n
-        result = best_sum(remainder, numbers, memo)
+        result = best_sum_memo(remainder, numbers, memo)
         if result is not None:
             attempt = [*result, n]
             if shortest is None or len(attempt) < len(shortest):
@@ -74,33 +74,36 @@ def best_sum(target, numbers, memo=None):
 
 
 def run_can_sum():
-    assert_equal(can_sum(5, [3, 4]), False)
-    assert_equal(can_sum(7, [2, 3, 5, 7]), True)
-    assert_equal(can_sum(8, [2, 3, 5, 7]), True)
-    assert_equal(can_sum(13, [2, 3, 5, 7]), True)
-    assert_equal(can_sum(7, [2, 4]), False)
-    assert_equal(can_sum(300, [7, 14]), False)
+    assert_equal(can_sum_memo(5, [3, 4]), False)
+    assert_equal(can_sum_memo(7, [2, 3, 5, 7]), True)
+    assert_equal(can_sum_memo(8, [2, 3, 5, 7]), True)
+    assert_equal(can_sum_memo(13, [2, 3, 5, 7]), True)
+    assert_equal(can_sum_memo(7, [2, 4]), False)
+    assert_equal(can_sum_memo(300, [7, 14]), False)
 
 
 def run_has_sum():
-    assert_equal(has_sum(5, [3, 4]), None)
-    assert_equal(has_sum(7, [2, 3, 5, 7]), [3, 2, 2])
-    assert_equal(has_sum(8, [2, 3, 5, 7]), [2, 2, 2, 2])
-    assert_equal(has_sum(13, [3, 5, 7]), [7, 3, 3])
-    assert_equal(has_sum(7, [2, 4]), None)
-    assert_equal(has_sum(300, [7, 14]), None)
-    assert_equal(has_sum(100, [1, 5, 10, 25]), [25, 25, 25, 25])
+    assert_equal(has_sum_memo(5, [3, 4]), None)
+    assert_equal(has_sum_memo(7, [2, 3, 5, 7]), [3, 2, 2])
+    assert_equal(has_sum_memo(8, [2, 3, 5, 7]), [2, 2, 2, 2])
+    assert_equal(has_sum_memo(13, [3, 5, 7]), [7, 3, 3])
+    assert_equal(has_sum_memo(7, [2, 4]), None)
+    assert_equal(has_sum_memo(300, [7, 14]), None)
+    assert_equal(has_sum_memo(100, [10, 25]), [10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
 
 
 def run_best_sum():
-    assert_equal(best_sum(5, [3, 4]), None)
-    assert_equal(best_sum(7, [2, 3, 5, 7]), [7])
-    assert_equal(best_sum(8, [2, 3, 5, 7]), [5, 3])
-    assert_equal(best_sum(8, [1, 4, 5]), [4, 4])
-    assert_equal(best_sum(13, [3, 5, 7]), [7, 3, 3])
-    assert_equal(best_sum(7, [2, 4]), None)
-    assert_equal(best_sum(300, [7, 14]), None)
+    assert_equal(best_sum_memo(5, [3, 4]), None)
+    assert_equal(best_sum_memo(7, [2, 3, 5, 7]), [7])
+    assert_equal(best_sum_memo(8, [2, 3, 5, 7]), [5, 3])
+    assert_equal(best_sum_memo(8, [1, 4, 5]), [4, 4])
+    assert_equal(best_sum_memo(13, [3, 5, 7]), [7, 3, 3])
+    assert_equal(best_sum_memo(7, [2, 4]), None)
+    assert_equal(best_sum_memo(300, [7, 14]), None)
+    assert_equal(best_sum_memo(100, [1, 5, 10, 15, 25]), [25, 25, 25, 25])
 
 
 if __name__ == "__main__":
+    run_can_sum()
+    run_has_sum()
     run_best_sum()
