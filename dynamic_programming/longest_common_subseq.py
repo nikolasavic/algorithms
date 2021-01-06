c2d82b7abbb5a8aeeece1f8e486add9567cd7239
Nikola Savic <nikolasavic@gmail.com>
2021-01-05 22:23:29 -0500

    Fix bug

--- dynamic_programming/longest_common_subseq.py
+++ dynamic_programming/longest_common_subseq.py
@@ -2,12 +2,12 @@ from utils import assert_equal
 
 
 def lcs_length_iter(alpha, beta):
-    dp = [[0 for _ in range(len(beta))] for _ in range(len(alpha))]
+    dp = [[0 for _ in range(len(beta) + 1)] for _ in range(len(alpha) + 1)]
 
-    for a in range(1, len(alpha)):
-        for b in range(1, len(beta)):
-            if alpha[a] == beta[b]:
-                dp[a][b] = 1 + max(dp[a - 1][b], dp[a][b - 1])
+    for a in range(1, len(alpha) + 1):
+        for b in range(1, len(beta) + 1):
+            if alpha[a - 1] == beta[b - 1]:
+                dp[a][b] = 1 + dp[a - 1][b - 1]
             else:
                 dp[a][b] = max(dp[a - 1][b], dp[a][b - 1])
 
@@ -35,14 +35,16 @@ def run_lcs_length():
     assert_equal(lcs_length_recursive("xxxxxXX", "yyyyyXX"), 2)
     assert_equal(lcs_length_recursive("xxxxxXxxX", "yyyyyXyyX"), 2)
     assert_equal(lcs_length_recursive("xxXxxXxxxX", "yyXyyyXyyX"), 3)
+    assert_equal(lcs_length_recursive("axxxa", "ayyya"), 2)
 
-    # assert_equal(lcs_length_iter("aza", "aaa"), 2)
-    # assert_equal(lcs_length_iter("abcdefgh", "xxbxdxexhxx"), 4)
-    # assert_equal(lcs_length_iter("xxooxxoooxx", "YoxYxoYoxxYYY"), 7)
-    # assert_equal(lcs_length_iter("abcdexxxabcxxx", "yyyyxxxabde"), 5)
-    # assert_equal(lcs_length_iter("xxxxxXX", "yyyyyXX"), 2)
-    # assert_equal(lcs_length_iter("xxxxxXxxX", "yyyyyXyyX"), 2)
-    # assert_equal(lcs_length_iter("xxXxxXxxxX", "yyXyyyXyyX"), 3)
+    assert_equal(lcs_length_iter("aza", "aaa"), 2)
+    assert_equal(lcs_length_iter("abcdefgh", "xxbxdxexhxx"), 4)
+    assert_equal(lcs_length_iter("xxooxxoooxx", "YoxYxoYoxxYYY"), 7)
+    assert_equal(lcs_length_iter("abcdexxxabcxxx", "yyyyxxxabde"), 5)
+    assert_equal(lcs_length_iter("xxxxxXX", "yyyyyXX"), 2)
+    assert_equal(lcs_length_iter("xxxxxXxxX", "yyyyyXyyX"), 2)
+    assert_equal(lcs_length_iter("xxXxxXxxxX", "yyXyyyXyyX"), 3)
+    assert_equal(lcs_length_iter("axxxa", "ayyya"), 2)
 
 
 if __name__ == "__main__":
