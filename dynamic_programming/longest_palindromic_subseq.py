d913ab5616b902da2a626b1a2561964420141bec
Nikola Savic <nikolasavic@gmail.com>
2021-01-21 09:58:51 -0500

    Add longest palindromic solution

--- dynamic_programming/longest_palindromic_subseq.py
+++ dynamic_programming/longest_palindromic_subseq.py
@@ -2,7 +2,23 @@ from utils import assert_equal
 
 
 def lps(string):
-    pass
+    n = len(string)
+    dp = [[0 for _ in range(n)] for _ in range(n)]
+
+    for x in range(n):
+        dp[x][x] = 1
+
+    for seq_len in range(2, n + 1):
+        for i in range(n - seq_len + 1):
+            j = i + seq_len - 1
+            if string[i] == string[j] and seq_len == 2:
+                dp[i][j] = 2
+            elif string[i] == string[j]:
+                dp[i][j] = 2 + dp[i + 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+
+    return dp[0][n-1]
 
 
 def run_lps():
