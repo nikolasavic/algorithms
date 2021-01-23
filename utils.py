6fff96486e3767f4fe231de2b407fb0d565ca583
Nikola Savic <nikolasavic@gmail.com>
2021-01-23 11:38:30 -0500

    Keep track of problem num in assertion

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
-def assert_equal(result, expected):
+def assert_equal(result, expected, prob=[1]):
     if result == expected:
+        print(f"#{len(prob)}  ✅")
+        prob.append(1)
         return True
     else:
-        print(f"expected: {expected}")
-        print(f"received: {result}")
+        print(f"#{len(prob)}  ❌")
+        print(f"  expected: {expected}")
+        print(f"  received: {result}")
+        prob.append(1)
         return False
