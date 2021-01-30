4e8d03060dab50bc8a96d8014c17402a3c64e8a1
Nikola Savic <nikolasavic@fastmail.com>
2021-01-30 15:33:17 -0500

    Start jumping frog

---
+++ dynamic_programming/jumping_frog.py
@@ -0,0 +1,28 @@
+from utils import assert_equal
+
+
+def linear_frog(n):
+    # Jumps:
+    # i -> i + 1
+    # i -> i + 4
+    pass
+
+
+def grid_frog(n):
+    # Jumps:
+    # i, j -> i + 1, j + 2
+    # i, j -> i + 2, j + 1
+    pass
+
+
+def run_linear_frog():
+    pass
+
+
+def run_grid_frog():
+    pass
+
+
+if __name__ == "__main__":
+    run_linear_frog()
+    run_grid_frog()
