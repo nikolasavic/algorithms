f514223d8df3706c445a70ae6c025cc14b6981d9
Nikola Savic <nikolasavic@fastmail.com>
2021-01-31 11:33:05 -0500

    Start can make change k coins

--- dynamic_programming/make_change.py
+++ dynamic_programming/make_change.py
@@ -13,6 +13,10 @@ def can_make_change(target, coins):
     return T[target]
 
 
+def can_make_change_k_coins(target, coins, k):
+    pass
+
+
 def best_make_change(target, coins):
     T = [None for _ in range(target + 1)]
     T[0] = []
@@ -28,7 +32,7 @@ def best_make_change(target, coins):
     return T[target]
 
 
-def run_can_sum():
+def run_can_make_change():
     assert_equal(can_make_change(5, [3, 4]), False)
     assert_equal(can_make_change(7, [2, 3, 5, 7]), True)
     assert_equal(can_make_change(8, [2, 3, 5, 7]), True)
@@ -37,7 +41,7 @@ def run_can_sum():
     assert_equal(can_make_change(300, [7, 14]), False)
 
 
-def run_best_sum():
+def run_best_make_change():
     assert_equal(best_make_change(5, [3, 4]), None)
     assert_equal(best_make_change(7, [2, 3, 5, 7]), [7])
     assert_equal(best_make_change(8, [2, 3, 5, 7]), [3, 5])
@@ -49,6 +53,11 @@ def run_best_sum():
     assert_equal(best_make_change(8, [1, 4, 5]), [4, 4])
 
 
+def run_can_make_change_k_coins():
+    pass
+
+
 if __name__ == "__main__":
-    run_can_sum()
-    run_best_sum()
+    run_can_make_change()
+    run_best_make_change()
+    run_can_make_change_k_coins()
