c297856153b0206e0551e3da6eea734039349a76
Nikola Savic <nikolasavic@fastmail.com>
2021-02-03 10:59:35 -0500

    Refactor

--- dynamic_programming/longest_increasing_subseq.py
+++ dynamic_programming/longest_increasing_subseq.py
@@ -5,7 +5,7 @@ def lis(numbers):
     n = len(numbers)
     T = [1 for _ in range(n + 1)]
 
-    for i in range(1, n + 1):
+    for i in range(2, n + 1):
         for j in range(1, i):
             if numbers[j - 1] < numbers[i - 1]:  # Not 1-based index
                 if T[i] < T[j] + 1:
